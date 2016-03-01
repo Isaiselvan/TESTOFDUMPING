ab2c87a22783188f7b2f67d134fed6e72a756216
isaiselvan <mohanisai@gmail.com>
2016-03-01 08:27:03 -0500

    Lte.cpp

---
+++ ANALYSEPCAP/lteGtp.cpp
@@ -0,0 +1,67 @@
+#include "lteGtp.h"
+
+
+int LteProtoBase::parseGtp(libtrace_packet_t * pkt , libtrace_udp_t * udpPkt)
+{
+ //Check if the UPD payload is a GTPU
+ uint32_t rem =0;
+ char *Gtp_ptr= NULL;  
+ char gtpHrd[8]; //64 bits
+ GTPhrd gtpHeader;
+
+ Gtp_ptr = (char *)trace_get_payload_from_udp (udpPkt,&rem);
+ 
+ if(rem < 8 && !Gtp_ptr)
+     return -1; 
+
+ memcpy(gtpHrd, Gtp_ptr, 8) ;
+
+  // Check version
+     if ((gtpHrd[0] >> 5) != GtpVersion1) // 0-2 version total 8 >> 5  is 1st 3 bits
+     return (-1);
+
+ // Check Protocol Type
+     if ( ((gtpHrd[0] & 0x10) >> 4) == GTPv1) // 00010000
+     {                                     // interested in 4 bit
+        gtpHeader.m_version = 1;
+     }else if (((gtpHrd[0] & 0x10) >> 4) == GTPv0)
+     {
+        gtpHeader.m_version = 0;  
+     }else 
+      return -1; 
+ // length 
+    gtpHeader.m_length = (gtpHrd[2] << 8) | gtpHrd[3];
+    
+    // Store the TEID
+    gtpHeader.m_teid  = (gtpHrd[4] << 24) | (gtpHrd[5] << 16) |
+      				(gtpHrd[6] << 8)  | (gtpHrd[7]);
+    
+  //Protocol statistics 
+   m_totalpkts++;
+   m_totaldata += gtpHeader.m_length;
+   
+   
+     
+
+ return 0;
+}
+
+
+int LteProtoBase::addPkt(libtrace_packet_t * pkt, GTPhrd gtpHrd, libtrace_ipproto_t type)
+{
+
+}
+
+
+int LteProtoBase::getProtoBase(std::string node, libtrace_ipproto_t){
+
+}
+
+int LteProtoBase::cleardashB(int newtsrtime, int newendtime)
+{
+
+}
+
+void LteProtoBase::printstats(){
+
+}
