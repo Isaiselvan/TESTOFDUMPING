5a637f687c6f51befc29c51371b8a954705ad2b0
isaiselvan <mohanisai@gmail.com>
2016-03-28 05:25:03 -0400

    Simulator script for internet traffic

---
+++ ANALYSEPCAP/weblink.sh
@@ -0,0 +1,23 @@
+#!/bin/bash 
+
+
+cd /apps/opt/LIBTRACE/test/
+
+StartWeblink(){
+        COUNTER=0
+         while [  $COUNTER -lt 15 ]; do
+             echo The counter is $COUNTER
+            /usr/bin/wget www.google.com > /dev/null 2>&1 &
+            /usr/bin/wget www.youtube.com > /dev/null 2>&1 &
+            /usr/bin/wget www.netflix.com > /dev/null  2>&1 &
+            /usr/bin/wget www.yahoo.com > /dev/null 2>&1 &
+            let COUNTER=COUNTER+1 
+         done
+}
+
+Times=0
+while [ $Times -lt 600 ]; do
+StartWeblink
+sleep 3
+let Times=Times+1
+done
