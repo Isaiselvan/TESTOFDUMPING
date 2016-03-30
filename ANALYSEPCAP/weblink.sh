3c228bf05afd196974371f7ccb4e06c9d6204a07
isaiselvan <mohanisai@gmail.com>
2016-03-30 04:06:26 -0400

    Add new interval 30 to support splunk span time of 30sec

--- ANALYSEPCAP/weblink.sh
+++ ANALYSEPCAP/weblink.sh
@@ -6,7 +6,7 @@ cd /apps/opt/LIBTRACE/test/
 StartWeblink(){
         COUNTER=0
          while [  $COUNTER -lt 15 ]; do
-             echo The counter is $COUNTER
+            # echo The counter is $COUNTER
             /usr/bin/wget www.google.com > /dev/null 2>&1 &
             /usr/bin/wget www.youtube.com > /dev/null 2>&1 &
             /usr/bin/wget www.netflix.com > /dev/null  2>&1 &
@@ -16,7 +16,7 @@ StartWeblink(){
 }
 
 Times=0
-while [ $Times -lt 600 ]; do
+while [ $Times -lt 1200 ]; do
 StartWeblink
 sleep 3
 let Times=Times+1
